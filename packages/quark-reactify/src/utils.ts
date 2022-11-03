6ab9f462e7b94995e1819c442d2de43895353a35
xiongweijian <xiongweijian@baidu.com>
2022-11-03 10:25:54 +0800

    feat: using `Set`

--- packages/quark-reactify/src/utils.ts
+++ packages/quark-reactify/src/utils.ts
@@ -1,7 +1,9 @@
-const nativeEvents = /* #__PURE__ */ Object.keys(HTMLElement.prototype)
+const nativeEventSet = /* #__PURE__ */ new Set(
+  Object.keys(HTMLElement.prototype)
   .filter(eventName => eventName.startsWith('on'))
-  .map(eventName => eventName.slice(2));
+  .map(eventName => eventName.slice(2))
+);
 
 export function isNativeEvent(eventName: string) {
-  return nativeEvents.indexOf(eventName) !== -1;
+  return nativeEventSet.has(eventName);
 };
