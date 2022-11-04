9ad6d81127d95743de9d7e0b986712e7544cecc1
xushunfa459 <xushunfa459@hellobike.com>
2022-11-04 19:15:01 +0800

    feat: update config

--- scripts/push2GithubWebsite.js
+++ scripts/push2GithubWebsite.js
@@ -1,6 +1,6 @@
 const sh = require('shelljs');
 
-sh.exec('cd example && npm run build',  function(code, stdout, stderr) {
+sh.exec('cd example && npm i --legacy-peer-deps && npm run build',  function(code, stdout, stderr) {
   console.log('Exit code:', code);
   console.log('Program output:', stdout);
   console.log('Program stderr:', stderr);
