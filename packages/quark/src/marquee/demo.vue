87122fda45329839affc3927a271325752e9f717
NoTwoBoy <1244476905@qq.com>
2022-11-09 01:00:22 +0800

    feat: format code.

--- packages/quark/src/marquee/demo.vue
+++ packages/quark/src/marquee/demo.vue
@@ -21,56 +21,56 @@
 </template>
 
 <script>
-  import { ref, onBeforeMount } from "vue";
-  import { createComponent } from "@/utils/create";
-  const { createDemo, translate } = createComponent("marquee");
-  import { useTranslate } from "@/sites/assets/util/useTranslate";
+import { ref, onBeforeMount } from "vue";
+import { createComponent } from "@/utils/create";
+const { createDemo, translate } = createComponent("marquee");
+import { useTranslate } from "@/sites/assets/util/useTranslate";
 
-  export default createDemo({
-    setup() {
-      const isPaused = ref(false);
+export default createDemo({
+  setup() {
+    const isPaused = ref(false);
 
-      onBeforeMount(() => {
-        useTranslate({
-          "zh-CN": {
-            basic: "基础用法",
-            speed: "不同速度",
-            paused: "暂停",
-            pausedButton: {
-              pause: "暂停",
-              continue: "继续",
-            },
-            reverse: "反转方向",
-            content:
-              "2022 年我们见证了很多技术的新发展新变化：低代码的突然爆火、数据库的接连开源、芯片的短缺与自研、训练模型的参数突破",
-            alert: {
-              return: "返回",
-              clear: "清除",
-            },
+    onBeforeMount(() => {
+      useTranslate({
+        "zh-CN": {
+          basic: "基础用法",
+          speed: "不同速度",
+          paused: "暂停",
+          pausedButton: {
+            pause: "暂停",
+            continue: "继续",
           },
-          "en-US": {
-            basic: "Basic Usage",
-            speed: "Marqueen Speed",
-            paused: "Paused",
-            pausedButton: {
-              pause: "Pause",
-              continue: "Continue",
-            },
-            reverse: "Reverse Direction",
-            content:
-              "In 2022, we will witness many new developments and changes in technology: the sudden explosion of low-code, the continuous open source of databases, the shortage of chips and self-research, and the parameter breakthrough of training models",
-            alert: {
-              return: "Go Back",
-              clear: "Clear",
-            },
+          reverse: "反转方向",
+          content:
+            "2022 年我们见证了很多技术的新发展新变化：低代码的突然爆火、数据库的接连开源、芯片的短缺与自研、训练模型的参数突破",
+          alert: {
+            return: "返回",
+            clear: "清除",
           },
-        });
+        },
+        "en-US": {
+          basic: "Basic Usage",
+          speed: "Marqueen Speed",
+          paused: "Paused",
+          pausedButton: {
+            pause: "Pause",
+            continue: "Continue",
+          },
+          reverse: "Reverse Direction",
+          content:
+            "In 2022, we will witness many new developments and changes in technology: the sudden explosion of low-code, the continuous open source of databases, the shortage of chips and self-research, and the parameter breakthrough of training models",
+          alert: {
+            return: "Go Back",
+            clear: "Clear",
+          },
+        },
       });
-      return {
-        isPaused,
-        translate,
-      };
-    },
-  });
+    });
+    return {
+      isPaused,
+      translate,
+    };
+  },
+});
 </script>
 <style src="./demo.scss"></style>
