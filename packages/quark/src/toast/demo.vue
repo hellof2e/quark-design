79da41b10dbcd9e7a2dba9e4701a05bd0b5efb0c
xushunfa459 <xushunfa459@hellobike.com>
2022-11-08 12:03:55 +0800

    feat: eslint config

--- packages/quark/src/toast/demo.vue
+++ packages/quark/src/toast/demo.vue
@@ -1,32 +1,32 @@
 <template>
-	<div class="demo toast-demo">
-		<h2>{{ translate("functionCall") }}</h2>
-		<div class="quark-cell" @click="handleTextClick">
-			<div>Text {{ translate("textTip") }}</div>
-			<quark-icon-arrow-right size="20" name="right"></quark-icon-arrow-right>
-		</div>
-		<div class="quark-cell" @click="handleSuccessClick">
-			<div>Success {{ translate("successTip") }}</div>
-			<quark-icon-arrow-right size="20" name="right"></quark-icon-arrow-right>
-		</div>
-		<div class="quark-cell" @click="handleErrorClick">
-			<div>Error {{ translate("errorTip") }}</div>
-			<quark-icon-arrow-right size="20" name="right"></quark-icon-arrow-right>
-		</div>
-		<div class="quark-cell" @click="handleWarningClick">
-			<div>Warning {{ translate("warningTip") }}</div>
-			<quark-icon-arrow-right size="20" name="right"></quark-icon-arrow-right>
-		</div>
-		<div class="quark-cell" @click="handleLoadingClick">
-			<div>Loading {{ translate("loadingTip") }}</div>
-			<quark-icon-arrow-right size="20" name="right"></quark-icon-arrow-right>
-		</div>
-		<h2>{{ translate("closeCallback") }}</h2>
-		<div class="quark-cell" @click="cbClick">
-			<div>{{ translate("closeCallback") }}</div>
-			<quark-icon-arrow-right size="20" name="right"></quark-icon-arrow-right>
-		</div>
-	</div>
+  <div class="demo toast-demo">
+    <h2>{{ translate("functionCall") }}</h2>
+    <div class="quark-cell" @click="handleTextClick">
+      <div>Text {{ translate("textTip") }}</div>
+      <quark-icon-arrow-right size="20" name="right"></quark-icon-arrow-right>
+    </div>
+    <div class="quark-cell" @click="handleSuccessClick">
+      <div>Success {{ translate("successTip") }}</div>
+      <quark-icon-arrow-right size="20" name="right"></quark-icon-arrow-right>
+    </div>
+    <div class="quark-cell" @click="handleErrorClick">
+      <div>Error {{ translate("errorTip") }}</div>
+      <quark-icon-arrow-right size="20" name="right"></quark-icon-arrow-right>
+    </div>
+    <div class="quark-cell" @click="handleWarningClick">
+      <div>Warning {{ translate("warningTip") }}</div>
+      <quark-icon-arrow-right size="20" name="right"></quark-icon-arrow-right>
+    </div>
+    <div class="quark-cell" @click="handleLoadingClick">
+      <div>Loading {{ translate("loadingTip") }}</div>
+      <quark-icon-arrow-right size="20" name="right"></quark-icon-arrow-right>
+    </div>
+    <h2>{{ translate("closeCallback") }}</h2>
+    <div class="quark-cell" @click="cbClick">
+      <div>{{ translate("closeCallback") }}</div>
+      <quark-icon-arrow-right size="20" name="right"></quark-icon-arrow-right>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -35,80 +35,87 @@ const { createDemo, translate } = createComponent("toast");
 import { useTranslate } from "@/sites/assets/util/useTranslate";
 import { ref, onBeforeMount } from "vue";
 import QuarkToast from "./index.tsx";
-import '@quarkd/icons/lib/arrow-right'
+import "@quarkd/icons/lib/arrow-right";
 
 export default createDemo({
-	setup() {
-		const tag = ref(null);
-		onBeforeMount(() => {
-			useTranslate({
-				"zh-CN": {
-					functionCall: "函数式调用",
-					closeCallback: "关闭后回调函数",
-					textTip: "文字提示",
-					successTip: "成功提示",
-					errorTip: "失败提示",
-					warningTip: "警告提示",
-					loadingTip: "加载提示",
-					networkTip: "网络失败，请稍后再试～",
-					executeCallback: "回调函数执行",
-					close: "一秒后关闭",
-				},
-				"en-US": {
-					functionCall: "Function Call",
-					closeCallback: "Close Callback",
-					textTip: "Text Tip",
-					successTip: "Success Tip",
-					errorTip: "Error Tip",
-					warningTip: "Warning Tip",
-					loadingTip: "Loading Tip",
-					networkTip: "Network failed, please try again later~",
-					executeCallback: "Execute Callback",
-					close: "Close after a second",
-				},
-			});
-		});
-		// QuarkToast.allowMultiple()
-		const cbClick = () => {
-			QuarkToast.text(`${translate("networkTip")}`, {
-				close: () => QuarkToast.text(`${translate("executeCallback")}`),
-			});
-		};
-		const handleTextClick = () => {
-			QuarkToast.text(`${translate("networkTip")}`);
-		};
-		const handleSuccessClick = () => {
-			QuarkToast.success(`${translate("successTip")}`);
-		};
-		const handleErrorClick = () => {
-			QuarkToast.error(`${translate("errorTip")}`);
-		};
-		const handleWarningClick = () => {
-			QuarkToast.warning(`${translate("warningTip")}`);
-		};
-		const handleLoadingClick = () => {
-			const toast = QuarkToast.loading(`${translate("close")}`, {
-				duration: 0,
-				size: 40,
-			});
-			setTimeout(() => {
-				toast.hide();
-			}, 3000);
-		};
-		const tagClick = () => {
-			tag.value.show = true;
-		};
-		return {
-			tag,
-			cbClick,
-			handleTextClick,
-			handleSuccessClick,
-			handleErrorClick,
-			handleWarningClick,
-			handleLoadingClick,
-			translate,
-		};
-	},
+  setup() {
+    const tag = ref(null);
+    onBeforeMount(() => {
+      useTranslate({
+        "zh-CN": {
+          functionCall: "函数式调用",
+          closeCallback: "关闭后回调函数",
+          textTip: "文字提示",
+          successTip: "成功提示",
+          errorTip: "失败提示",
+          warningTip: "警告提示",
+          loadingTip: "加载提示",
+          networkTip: "网络失败，请稍后再试～",
+          executeCallback: "回调函数执行",
+          close: "一秒后关闭",
+        },
+        "en-US": {
+          functionCall: "Function Call",
+          closeCallback: "Close Callback",
+          textTip: "Text Tip",
+          successTip: "Success Tip",
+          errorTip: "Error Tip",
+          warningTip: "Warning Tip",
+          loadingTip: "Loading Tip",
+          networkTip: "Network failed, please try again later~",
+          executeCallback: "Execute Callback",
+          close: "Close after a second",
+        },
+      });
+    });
+
+    const cbClick = () => {
+      QuarkToast.text(`${translate("networkTip")}`, {
+        close: () => QuarkToast.text(`${translate("executeCallback")}`),
+      });
+    };
+
+    const handleTextClick = () => {
+      QuarkToast.text(`${translate("networkTip")}`);
+    };
+
+    const handleSuccessClick = () => {
+      QuarkToast.success(`${translate("successTip")}`);
+    };
+
+    const handleErrorClick = () => {
+      QuarkToast.error(`${translate("errorTip")}`);
+    };
+
+    const handleWarningClick = () => {
+      QuarkToast.warning(`${translate("warningTip")}`);
+    };
+
+    const handleLoadingClick = () => {
+      const toast = QuarkToast.loading(`${translate("close")}`, {
+        duration: 0,
+        size: 40,
+      });
+      setTimeout(() => {
+        toast.hide();
+      }, 3000);
+    };
+
+    // const tagClick = () => {
+    //   tag.value.show = true;
+    // };
+
+    return {
+      tag,
+      cbClick,
+      handleTextClick,
+      handleSuccessClick,
+      handleErrorClick,
+      handleWarningClick,
+      handleLoadingClick,
+      translate,
+    };
+  },
 });
 </script>
 <style src="./demo.scss"></style>
