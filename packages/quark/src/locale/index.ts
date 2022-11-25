7471c1a55b6759c5cc2608fa55558d5891018e37
王炎 <yan.wang@qiyuesuo.com>
2022-11-25 18:00:08 +0800

    fix(locale): fix typos

--- packages/quark/src/locale/index.ts
+++ packages/quark/src/locale/index.ts
@@ -12,8 +12,8 @@ export class Local {
   static use(newLang: Lange) {
     this.current = newLang;
   }
-  static add(messgae: Lange) {
-    const tempCurrent = assign({}, this.current, messgae);
+  static add(message: Lange) {
+    const tempCurrent = assign({}, this.current, message);
     this.current = tempCurrent;
   }
 }
