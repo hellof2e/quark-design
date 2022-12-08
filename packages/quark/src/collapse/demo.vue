f76eeb2e0ddd2396a4acd8a8f5821832acb32fa2
xxxsf <xushunfa459@hellobike.com>
2022-12-08 18:56:08 +0800

    release: add collapse

--- packages/quark/src/collapse/demo.vue
+++ packages/quark/src/collapse/demo.vue
@@ -3,37 +3,44 @@
     <h2>{{ translate("basic") }}</h2>
     <div>
       <quark-collapse :title="translate('title1')">
-        生命远不止连轴转和忙到极限，人类的体验远比这辽阔、丰富得多。
+        {{ translate("content") }}
       </quark-collapse>
       <quark-collapse :title="translate('title2')">
-        生命远不止连轴转和忙到极限，人类的体验远比这辽阔、丰富得多。
+        {{ translate("content") }}
+      </quark-collapse>
+    </div>
+
+    <h2>{{ translate("openState") }}</h2>
+    <div>
+      <quark-collapse :title="translate('title')" open>
+        {{ translate("content") }}
       </quark-collapse>
     </div>
 
     <h2>{{ translate("hideicon") }}</h2>
     <div>
-      <quark-collapse :title="translate('title1')" hideicon>
-        生命远不止连轴转和忙到极限，人类的体验远比这辽阔、丰富得多。
+      <quark-collapse :title="translate('title')" hideicon>
+        {{ translate("content") }}
       </quark-collapse>
     </div>
 
     <h2>{{ translate("customTitile") }}</h2>
     <div>
-      <quark-collapse :title="translate('title1')">
+      <quark-collapse :title="translate('title')">
         <div slot="title">
           <span style="color: blueviolet"
-            >{{ translate("title1") }}{{ translate("title1") }}</span
+            >{{ translate("title") }}{{ translate("title") }}</span
           >
         </div>
-        生命远不止连轴转和忙到极限，人类的体验远比这辽阔、丰富得多。
+        {{ translate("content") }}
       </quark-collapse>
     </div>
 
     <h2>{{ translate("customIcon") }}</h2>
     <div>
-      <quark-collapse :title="translate('title1')">
-        生命远不止连轴转和忙到极限，人类的体验远比这辽阔、丰富得多。
+      <quark-collapse :title="translate('title')">
         <span slot="icon" class="collapse-icon">🎉🎉🎉</span>
+        {{ translate("content") }}
       </quark-collapse>
     </div>
   </div>
@@ -52,19 +59,27 @@ export default createDemo({
       useTranslate({
         "zh-CN": {
           basic: "基本用法",
+          openState: "打开状态",
           hideicon: "无icon样式",
           customTitile: "自定义标题",
           customIcon: "自定义图标",
+          title: "标题",
           title1: "标题1",
           title2: "标题2",
+          content:
+            "生命远不止连轴转和忙到极限，人类的体验远比这辽阔、丰富得多。",
         },
         "en-US": {
           basic: "Basic Usage",
+          openState: "Open State",
           hideicon: "Custom Style",
           customTitile: "Custom titile",
           customIcon: "Custom Icon",
+          title: "Title",
           title1: "Title1",
           title2: "Title2",
+          content:
+            "  Life is far more than spinning around and being busy to the limit. Human experience is far broader and richer than this.",
         },
       });
     });
