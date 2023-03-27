2120ff3ca98abe6a22b5ae284147717b572d1be3
xxxsf <xushunfa459@hellobike.com>
2023-03-27 19:19:46 +0800

    feat: 优化demo

--- example/src/sites/mobile-vue/App.vue
+++ example/src/sites/mobile-vue/App.vue
@@ -128,12 +128,11 @@ export default defineComponent({
 	}
 
 	.demo {
-		// height: 100%;
 		height: 100vh;
 		background: #f5f5f9;
 		overflow-x: hidden;
 		overflow-y: auto;
-		padding: 70px 18px 18px;
+		padding: 70px 18px;
 		min-height: 668px;
 
 		&.no-padding {
