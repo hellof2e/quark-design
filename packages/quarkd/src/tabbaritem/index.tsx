0d73833e715be228afdbd09f9e85dcd2f523abca
xsf0105 <allan91coder@gmail.com>
2023-04-28 15:33:12 +0800

    fix: 优化tabbar组件

--- packages/quarkd/src/tabbaritem/index.tsx
+++ packages/quarkd/src/tabbaritem/index.tsx
@@ -1,6 +1,9 @@
 import QuarkElement, { customElement } from "quarkc";
 import style from "./style.css";
 
+// eslint-disable-next-line @typescript-eslint/no-empty-interface
+export interface Props {}
+
 @customElement({ tag: "quark-tabbar-item", style })
 class QuarkTabbarItem extends QuarkElement {
   render() {
