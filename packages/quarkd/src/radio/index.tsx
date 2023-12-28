a82d54ccaa76dcafb7a6cd3eee65641d25b4a2bd
Sanqi <sanqi_med@163.com>
2023-12-28 17:18:00 +0800

    fix: radio check

--- packages/quarkd/src/radio/index.tsx
+++ packages/quarkd/src/radio/index.tsx
@@ -89,30 +89,10 @@ class QuarkRadio extends QuarkElement {
     return (
       <div class="quark-radio" onClick={this.handleCheck}>
         <span class="quark-radio-container">
-          <svg
+          <img
             class="quark-radio-icon"
-            xmlns="http://www.w3.org/2000/svg"
-            width="40px"
-            height="40px"
-            viewBox="0 0 40 40"
-            version="1.1"
-          >
-            <title>画板</title>
-            <g
-              id="画板"
-              stroke="none"
-              stroke-width="1"
-              fill="none"
-              fill-rule="evenodd"
-            >
-              <path
-                d="M28.5857864,13.5857864 C29.366835,12.8047379 30.633165,12.8047379 31.4142136,13.5857864 C32.1952621,14.366835 32.1952621,15.633165 31.4142136,16.4142136 L19.9142136,27.9142136 C19.133165,28.6952621 17.866835,28.6952621 17.0857864,27.9142136 L10.5857864,21.4142136 C9.80473785,20.633165 9.80473785,19.366835 10.5857864,18.5857864 C11.366835,17.8047379 12.633165,17.8047379 13.4142136,18.5857864 L18.5,23.671 L28.5857864,13.5857864 Z"
-                id="路径-5"
-                fill="#FFFFFF"
-                fill-rule="nonzero"
-              />
-            </g>
-          </svg>
+            src="https://m.hellobike.com/resource/quark/images/Kr6fLjveFHjjvVXIfDwye.png"
+          />
         </span>
         <label class={this.classNames}>
           <slot ref={this.slotRef} onslotchange={this.dealClass}></slot>
